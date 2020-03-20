85594e51a87e317da8586d165dfcf509167a904a
Jason Mimick <jmimick@gmail.com>
2020-03-20 08:30:48 -0400

    working e-2-e

--- clusters/k3sup-gce.sh
+++ clusters/k3sup-gce.sh
@@ -1,59 +1,82 @@
 #!/bin/bash
 
 set -u
-
-ORG="mongodb-k3s"
-NEW_TAG=$( curl -s https://frightanic.com/goodies_content/docker-names.php | tr '_' '-' )
-NEW_CLUSTER_TAG="${ORG}-${NEW_TAG}"
-
-CLUSTER_TAG=${CLUSTER_TAG:-${NEW_CLUSTER_TAG}}
-
+set -x
 echo "Installing MongoDB Enterprise Data Services Cluster"
 echo "Powered by Google Compute Engine"
 echo "mongodb-k3s sandbox demonstration kit"
 echo "For research & development purposes only."
 echo "Settings:"
-INSTANCE_TYPE=${WORKER_INSTANCE_TYPE:-n1-standard-1}
-OPS_MANAGER_INSTANCE_TYPE=${MANAGER_INSTANCE_TYPE:-n1-standard-8}
-ZONE=${ZONE:-us-central1-b}
-ZONE_DR=${ZONE_DR:-us-central1-c}
+ORG="mongodb-k3s"
+NEW_TAG=$( curl -s https://frightanic.com/goodies_content/docker-names.php | tr '_' '-' )
+NEW_CLUSTER_TAG="${ORG}-${NEW_TAG}"
+CLUSTER_TAG=${CLUSTER_TAG:-${NEW_CLUSTER_TAG}}
+DB_WORKER_INSTANCE_TYPE=${DB_WORKER_INSTANCE_TYPE:-n1-standard-1}
+OPS_MANAGER_DB_WORKER_INSTANCE_TYPE=${OPS_MANAGER_DB_WORKER_INSTANCE_TYPE:-n1-standard-8}
+ZONE_A=${ZONE_A:-us-central1-b}
+ZONE_B=${ZONE_B:-us-central1-c}
 MMS_NODES_TAG="${CLUSTER_TAG}-ops-manager"
-
-echo "CLUSTER_TAG=${CLUSTER_TAG}"
-echo "ZONE=${ZONE}"
-echo "ZONE_DR=${ZONE_DR}"
-echo "INSTANCE_TYPE=${INSTANCE_TYPE}"
-echo "OPS_MANAGER_INSTANCE_TYPE=${OPS_MANAGER_INSTANCE_TYPE}"
-echo "MMS_NODES_TAG=${MMS_NODES_TAG}"
+NUM_DB_WORKER_NODES_ZONE_A=3
+NUM_DB_WORKER_NODES_ZONE_B=2
+MMS_NODES_ZONE_A=2
+MMS_NODES_ZONE_B=2
 
 up() {
     (
     set -x
+
+    echo "Creating MASTER node"
     gcloud compute instances create "${CLUSTER_TAG}-master" \
-        --machine-type "${INSTANCE_TYPE}" \
-        --zone "${ZONE}" \
-        --tags "${ORG}","${CLUSTER_TAG}","${CLUSTER_TAG}-master"
+        --machine-type "${DB_WORKER_INSTANCE_TYPE}" \
+        --zone="${ZONE_A}" --tags "${ORG}","${CLUSTER_TAG}","${CLUSTER_TAG}-master"
+    
 
-    gcloud compute instances create \
-        "${CLUSTER_TAG}-worker-0" "${CLUSTER_TAG}-worker-1" "${CLUSTER_TAG}-worker-2" \
-        --machine-type "${INSTANCE_TYPE}" \
-        --zone "${ZONE}" \
-        --tags "${ORG}","${CLUSTER_TAG}","${CLUSTER_TAG}-worker" 
+    echo "Creating ${NUM_DB_WORKER_NODES_ZONE_A} workers in ${ZONE_A}"
+    echo "---------------------------------------------------------"
+    for i in $(seq 1 ${NUM_DB_WORKER_NODES_ZONE_A}); do
+      gcloud compute instances create \
+          "${CLUSTER_TAG}-worker-a-${i}" \
+          --machine-type "${DB_WORKER_INSTANCE_TYPE}" \
+          --zone="${ZONE_A}" \
+          --tags "${ORG}","${CLUSTER_TAG}","${CLUSTER_TAG}-worker","${CLUSTER_TAG}-worker-a"
+    done
 
-    gcloud compute instances create "${MMS_NODES_TAG}-0" \
-        --machine-type "${OPS_MANAGER_INSTANCE_TYPE}" \
-        --zone "${ZONE}" \
-        --tags "${ORG}","${CLUSTER_TAG}","${MMS_NODES_TAG}","${MMS_NODES_TAG}-${ZONE}"
+    echo "Creating ${NUM_DB_WORKER_NODES_ZONE_B} workers in ${ZONE_B}"
+    echo "---------------------------------------------------------"
+    for i in $(seq 1 ${NUM_DB_WORKER_NODES_ZONE_B}); do
+      gcloud compute instances create \
+          "${CLUSTER_TAG}-worker-b-${i}" \
+          --machine-type "${DB_WORKER_INSTANCE_TYPE}" \
+          --zone="${ZONE_B}" \
+          --tags "${ORG}","${CLUSTER_TAG}","${CLUSTER_TAG}-worker","${CLUSTER_TAG}-worker-b"
+    done
 
-    gcloud compute instances create "${MMS_NODES_TAG}-1" \
-        --machine-type "${OPS_MANAGER_INSTANCE_TYPE}" \
-        --zone "${ZONE}" \
-        --tags "${ORG}","${CLUSTER_TAG}","${MMS_NODES_TAG}","${MMS_NODES_TAG}-${ZONE}"
 
-    gcloud compute instances create "${MMS_NODES_TAG}-2" \
-        --machine-type "${OPS_MANAGER_INSTANCE_TYPE}" \
-        --zone "${ZONE_DR}" \
-        --tags "${ORG}","${CLUSTER_TAG}","${MMS_NODES_TAG}","${MMS_NODES_TAG}-${ZONE_DR}"
+    echo "Creating ${MMS_NODES_ZONE_A} Ops Manager nodes in ${ZONE_A}"
+    echo "---------------------------------------------------------"
+    for i in $(seq 1 ${NUM_OPS_MANAGER_NODES}); do
+    gcloud compute instances create "${MMS_NODES_TAG}-a-${i}" \
+        --machine-type "${OPS_MANAGER_DB_WORKER_INSTANCE_TYPE}" \
+        --zone="${ZONE_A}" \
+        --tags "${ORG}","${CLUSTER_TAG}","${MMS_NODES_TAG}","${MMS_NODES_TAG}-a"
+    done
+
+    echo "Creating ${MMS_NODES_ZONE_B} Ops Manager nodes in ${ZONE_B}"
+    echo "---------------------------------------------------------"
+    for i in $(seq 1 ${MMS_NODES_ZONE_B}); do
+    gcloud compute instances create "${MMS_NODES_TAG}-b-${i}" \
+        --machine-type "${OPS_MANAGER_DB_WORKER_INSTANCE_TYPE}" \
+        --zone="${ZONE_B}" \
+        --tags "${ORG}","${CLUSTER_TAG}","${MMS_NODES_TAG}","${MMS_NODES_TAG}-b"
+    done
+
+
+    echo "Creating 1 Ops Manager Backup Daemon in ${ZONE_B}"
+    echo "---------------------------------------------------------"
+    gcloud compute instances create "${MMS_NODES_TAG}-backup-daemon" \
+        --machine-type "${OPS_MANAGER_DB_WORKER_INSTANCE_TYPE}" \
+        --zone="${ZONE_B}" \
+        --tags "${ORG}","${CLUSTER_TAG}","${MMS_NODES_TAG}","${MMS_NODES_TAG}-${ZONE_B}","${MMS_NODES_TAG}-backup-daemon"
 
     gcloud compute config-ssh
     )
@@ -73,6 +96,10 @@ up() {
                   --allow=tcp:6443 \
                   --target-tags="${CLUSTER_TAG}"
 
+    echo "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"
+    echo "k3sup for: $(gcloud compute instances list --filter=tags.items="${CLUSTER_TAG}-worker" --format="get(name)")"
+    echo "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww"
+
     gcloud compute instances list \
         --filter=tags.items="${CLUSTER_TAG}-worker" \
         --format="get(networkInterfaces[0].accessConfigs.natIP)" | \
@@ -81,6 +108,12 @@ up() {
             --ssh-key ~/.ssh/google_compute_engine \
             --user $(whoami) \
             --ip
+
+    echo "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm"
+    echo "k3sup for: $(gcloud compute instances list \
+         --filter=tags.items="${MMS_NODES_TAG}" \
+         --format="get(name)")"
+    echo "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww"
 
     gcloud compute instances list \
         --filter=tags.items="${MMS_NODES_TAG}" \
@@ -92,10 +125,34 @@ up() {
             --ip
     )
 
+    echo "mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm: sleep 5 seconds."
+    sleep 5
     export KUBECONFIG=`pwd`/kubeconfig
     kubectl get nodes
-    kubectl label node ${MMS_NODES_TAG}-{0,1,2} kubernetes.io/role=mongodb-ops-manager
-    kubectl label node ${CLUSTER_TAG}-worker-{0,1,2} kubernetes.io/role=mongodb-node
+
+    echo "Adding labels to nodes. This controls how MongoDB components are deployed."
+    echo "---------------------------------------------------------"
+    echo "MongoDB Cluster Kubernetes Worker Nodes (${NUM_DB_WORKER_NODES_ZONE_A}) in ZONE_A=${ZONE_A}"
+    for i in $(seq 1 ${NUM_DB_WORKER_NODES_ZONE_A}); do
+      kubectl label node "${CLUSTER_TAG}-worker-a-${i}" kubernetes.io/role=mongodb-node
+    done
+    echo "MongoDB Cluster Kubernetes Worker Nodes (${NUM_DB_WORKER_NODES_ZONE_B}) in ZONE_B=${ZONE_B}"
+    for i in $(seq 1 ${NUM_DB_WORKER_NODES_ZONE_B}); do
+      kubectl label node "${CLUSTER_TAG}-worker-b-${i}" kubernetes.io/role=mongodb-node
+    done
+
+    echo "MongoDB Ops Manager Kubernetes Worker Nodes (${MMS_NODES_ZONE_A}) in ZONE_A=${ZONE_A}"
+    for i in $(seq 1 ${MMS_NODES_ZONE_A}); do
+      kubectl label node "${MMS_NODES_TAG}-a-${i}" kubernetes.io/role=mongodb-ops-manager
+    done
+    echo "MongoDB Ops Manager Kubernetes Worker Nodes (${MMS_NODES_ZONE_B}) in ZONE_B=${ZONE_B}"
+    for i in $(seq 1 ${MMS_NODES_ZONE_B}); do
+      kubectl label node "${MMS_NODES_TAG}-b-${i}" kubernetes.io/role=mongodb-ops-manager
+    done
+
+    echo " MongoDB Ops Manager Backup Daemon Kubernetes Worker Node in- ZONE_B=${ZONE_B}"
+    kubectl label node ${MMS_NODES_TAG}-backup-daemon kubernetes.io/role=mongodb-ops-manager-backup-daemon
+    echo "---------------------------------------------------------"
 
 }
 
--- mongodb-k3sup-gcp.sh
+++ mongodb-k3sup-gcp.sh
@@ -2,6 +2,11 @@
 
 set -u
 . ./clusters/k3sup-gce.sh up
+
+# Kubernetes Dashboard
+kubectl apply -f addons/dashboard/*.yaml
+
+# MongoDB Enterprise Data Services
 kubectl create ns mongodb
 helm install -n mongodb mongodb .
 
